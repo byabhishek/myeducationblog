6b430dc1809495b0effc1fbce7ee89ae9a1ff217
byabhishekpinservices <abhishek@pinservices.in>
2023-05-19 15:18:02 +0530

    dd

--- db/migrate/20230518075733_change_column_of_avatar.rb
+++ db/migrate/20230518075733_change_column_of_avatar.rb
@@ -1,6 +1,6 @@
 class ChangeColumnOfAvatar < ActiveRecord::Migration[7.0]
   def change
-    change_column :blogs, :avatar, :binary 
+    change_column :blogs, :avatar, :binary, using: 'avatar::bytea' 
     rename_column :blogs, :avatar, :photo 
 
   end
