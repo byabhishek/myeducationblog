1195634790c62456f2aa2a21c4b8f2f2f11daaf5
byabhishekpinservices <abhishek@pinservices.in>
2023-05-20 22:09:33 +0530

    a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   def not_found
-    redirect_to root_path
+    redirect_to root_path, notice: 'No page found at that address'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   resources :blogs
   resources :users
   root "blogs#index"
-  get '*', to: 'application#not_found'
+  get '*all', to: 'application#not_found', constraints: lambda { |req|
+    req.path.exclude? 'rails/active_storage'
+  }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
